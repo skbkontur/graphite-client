11c7126ed001967f1308bbc8803994482d606215
Alexander Pahmutov <al.pahumtov@skbkontur.ru>
2016-10-23 17:00:00 +0500

    hostname resolver

---
+++ Graphite.Client/Pooling/Utils/HostnameResolverWithCache.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace SKBKontur.Graphite.Client.Pooling.Utils
+{
+    public class HostnameResolverWithCache
+    {
+        private readonly TimeSpan cacheDuration;
+        private DateTime lastResolveTime;
+        private IPAddress lastResolveResult;
+        private string lastResolvedHostname;
+        public bool LastResultFromCache { get; private set; }
+
+        public HostnameResolverWithCache(TimeSpan cacheDuration)
+        {
+            this.cacheDuration = cacheDuration;
+        }
+
+        public IPAddress Resolve(string hostname)
+        {
+            if (CacheIsValid(hostname))
+            {
+                return lastResolveResult;
+            }
+
+            lastResolveTime = DateTime.UtcNow;
+            lastResolvedHostname = hostname;
+
+            if (string.IsNullOrEmpty(hostname))
+                return lastResolveResult = null;
+
+            try
+            {
+                var ipAddresses = Dns.GetHostAddresses(hostname);
+                lastResolveResult = ipAddresses.FirstOrDefault();
+            }
+            catch (Exception)
+            {
+                lastResolveResult = null;
+            }
+            return lastResolveResult;
+        }
+
+        private bool CacheIsValid(string hostname)
+        {
+            return LastResultFromCache = lastResolveTime.Add(cacheDuration) > DateTime.UtcNow && lastResolvedHostname == hostname;
+        }
+    }
+}
