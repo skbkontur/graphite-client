dfcd240a166361035d0e69fe2dab0a301b16a5e8
Alexander Pahmutov <al.pahumtov@skbkontur.ru>
2016-10-23 17:07:23 +0500

    тесты для проверки времени соединения

---
+++ Graphite.Client.Tests/ConnectionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+using NUnit.Framework;
+
+using SKBKontur.Graphite.Client.StatsD;
+
+namespace Graphite.Client.Tests
+{
+    public class ConnectionTests
+    {
+        [TestCase("non-exists")]
+        [TestCase("graphite-test")]
+        public void TestConnection(string hostname)
+        {
+            var topology = new TestGraphiteTopology
+            {
+                Enabled = true,
+                StatsD = new DnsEndPoint(hostname, 8125)
+            };
+            var sut = new PooledStatsDClient(topology);
+            var stopwatch = Stopwatch.StartNew();
+            sut.Timing(15, 1.0, "Test");
+            var attempt1 = stopwatch.ElapsedMilliseconds;
+            stopwatch.Restart();
+            sut.Timing(15, 1.0, "Test");
+            var attempt2 = stopwatch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Host: {hostname}\nt1: {attempt1}\nt2: {attempt2}");
+        }
+    }
+}
---
+++ Graphite.Client.Tests/TestGraphiteTopology.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+using SKBKontur.Graphite.Client.Settings;
+
+namespace Graphite.Client.Tests
+{
+    public class TestGraphiteTopology : IGraphiteTopology
+    {
+        public bool Enabled { get; set; }
+        public DnsEndPoint StatsD { get; set; }
+        public DnsEndPoint Graphite { get; set; }
+        public GraphiteProtocol GraphiteProtocol { get; set; }
+        public string AnnotationsUrl { get; set; }
+    }
+}
